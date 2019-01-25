979b2e9554a4c1c5840ce1f5503b3e0bed7fd603
Pravinth Samuel Vethanayagam <pravinth.samuel@gmail.com>
2019-01-25 03:28:50 -0800

    Added Unet model. Added function to perform train test split.

--- src/data.py
+++ src/data.py
@@ -1,5 +1,8 @@
 import boto3
 import os
+import glob
+import subprocess
+from sklearn.model_selection import train_test_split
 
 def s3_list_subfolders(s3_bucket, s3_path):
     client = boto3.client('s3')
@@ -17,6 +20,24 @@ def s3_download_folder(s3_bucket, s3_path, local_dest):
         client.download_file(s3_bucket, res.get('Key'), dest_pathname)
         print('Downloaded : {}'.format(res.get('Key')))
 
+
+BLENDER_COMMAND = 'blender --background --python ./src/render_blender.py -- --views 10 --output_folder ./data/processed/{} {}'
+
+def generate_depth_2d_images():
+    for file_name in glob.glob('data/raw/**/*.obj', recursive=True):
+        synset = file_name.split('/')[2]
+        model_number = file_name.split('/')[3]
+
+        print('Generating for model number : {}'.format(model_number))
+        os.system(BLENDER_COMMAND.format(os.path.join(synset, model_number), file_name) + ' > /dev/null')
+
+def train_test_filenames():
+    output_files = glob.glob('./data/processed/**/*depth*.png', recursive=True)
+    input_files = [f.replace('_depth', '').split('.')[1]+'.png' for f in output_files]
+
+    return train_test_split(input_files, output_files, test_size=0.2)
+
+
 if __name__ == '__main__':
 
     # Test code which downloads 5 different models from the shapenet dataset
@@ -24,3 +45,4 @@ if __name__ == '__main__':
         print('Downloading from {}'.format(folder))
         s3_download_folder('shapenet-dataset', folder, 'data/raw')
         print()
+    generate_depth_2d_images()
